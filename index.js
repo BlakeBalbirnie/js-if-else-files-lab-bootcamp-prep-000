200d427e551e0345f10b217d47b055dd5210d3af
Blake Balbirnie <blakebalbirnie@gmail.com>
2017-10-18 19:49:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
 if (name === "Susan") {
     console.log("The name is correct")
 } else {
     console.log("Expected the name to be Susan")
-}
-
-if (height === 70) {
-    console.log("The height is correct")
-} else {
-    console.log("Expected: 70, Received: "+index.height)
 }
 
 // Don't worry about this
